14d5ddcf7e8c65eb6ada2cf20e17a638f0c9e716
Yurii Sedow <sedow94@mail.ru>
2022-11-17 19:38:57 +0300

    add edit btn

--- app.js
+++ app.js
@@ -19,20 +19,20 @@ inputDiv.classList.add("text-bg-warning", "p-3", "d-flex", "justify-content-betw
 //Инпут для ввода задачи
 const input = document.createElement("input");
 input.classList.add("rounded-pill", "p-2", "border-success", "w-75");
-input.placeholder = "Ввести название дела";
+input.placeholder = "What do you have planned?";
 input.addEventListener("focus", () => {
   input.placeholder = "";
 });
 
 //Кнопка для добаления дела
 const btn = document.createElement("button");
-btn.innerText = "Добавить";
+btn.innerText = "Add";
 btn.classList.add("btn", "btn-success");
 btn.setAttribute("role", "submit");
 
 //Подзаголовок для имеющихся дел
 const headingBlock = document.createElement("h4");
-headingBlock.innerText = "Список моих дел:";
+headingBlock.innerText = "My To-Do List:";
 headingBlock.classList.add("mt-3");
 headingBlock.style.fontFamily = "'Poppins', sans-serif";
 headingBlock.style.fontWeight = "300";
@@ -40,7 +40,7 @@ headingBlock.style.color = `${colors.yellow}`;
 
 //Список для вывода дел
 const list = document.createElement("div");
-list.style.listStyle = "none";
+
 //Вставка элементов
 inputDiv.append(input);
 inputDiv.append(btn);
@@ -51,17 +51,41 @@ container.append(list);
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   const inputValue = input.value;
+  if (!inputValue) {
+    alert("Enter your task");
+    return;
+  }
   const listItem = document.createElement("div");
+  const listItem__el = document.createElement("input");
+  listItem__el.value = inputValue;
+  listItem__el.classList.add("w-100", "text-bg-success", "text-white-50", "p-2");
+  listItem__el.style.marginRight = "20px";
+  listItem__el.style.borderColor = `${colors.blue}`;
+  listItem__el.setAttribute("readonly", "readonly");
   listItem.classList.add("p-3", "d-flex", "justify-content-between", "align-items-center", "mb-2", "text-bg-success", "text-white-50");
   listItem.style.fontFamily = "'Poppins', sans-serif";
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("btn", "btn-primary");
+  editBtn.innerText = "Edit";
+  editBtn.style.marginRight = "20px";
+  editBtn.addEventListener("click", () => {
+    if (editBtn.innerText == "Edit") {
+      listItem__el.removeAttribute("readonly");
+      listItem__el.focus();
+      editBtn.innerText = "Save";
+    } else {
+      listItem__el.setAttribute("readonly", "readonly");
+      editBtn.innerText = "Edit";
+    }
+  });
   const deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("role", "button");
-  deleteBtn.innerText = "Удалить";
+  deleteBtn.innerText = "Delete";
   deleteBtn.classList.add("btn", "btn-danger");
   deleteBtn.addEventListener("click", () => {
     listItem.remove();
   });
   list.append(listItem);
-  listItem.append(inputValue, deleteBtn);
-  input.value = input.placeholder;
+  listItem.append(listItem__el, editBtn, deleteBtn);
+  input.value = "";
 });
